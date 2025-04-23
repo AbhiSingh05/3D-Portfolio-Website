84971eeba3a4ebde1a53b95d8378f9b53e9a4e60
Abhishek Singh <111077424+AbhiSingh05@users.noreply.github.com>
2025-04-23 23:34:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/3D-Portfolio-Website/', // 👈 this is the fix
   plugins: [react()],
 });
